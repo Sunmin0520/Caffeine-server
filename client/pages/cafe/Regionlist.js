01d33978689fd960344ff5cbec6678cce502ac7c
ayus91 <jihaleeb1@gmail.com>
2020-07-21 16:47:20 +0900

    Add axios, Navigation & Fix few codes

--- client/pages/cafe/Regionlist.js
+++ client/pages/cafe/Regionlist.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Button } from "react-native"
+import axios from "axios"
 
-const Regionlist = () => {
+function Regionlist({ navigation }) {
   //DB에 있는 지역리스트를 가져옵니다.
   return (
     <View style={styles.container}>
       <Text style={styles.textstyle}>어떤 지역의 카페정보가 궁금하신가요?</Text>
-      {Fakedata.map((data) => (
-        <View key={data.id}>
-          <Text style={styles.textstyle}>{data.region}</Text>
-        </View>
-      ))}
+      <View>
+        {axios
+          .get("http://localhost:4000/region")
+          .then((res) =>
+            res.map((data) => (
+              <View key={data.id}>
+                {/* 각각의 지역 목록을 버튼화하여 선택시 해당 지역의 카페 목록으로 이동합니다 */}
+                <Button
+                  title={data.name}
+                  style={styles.textstyle}
+                  onPress={() => {
+                    navigation.navigate("Region")
+                  }}
+                ></Button>
+              </View>
+            ))
+          )
+          .catch(function (error) {
+            console.log(error) //401{result:"token expired"}
+          })}
+      </View>
     </View>
   )
 }
