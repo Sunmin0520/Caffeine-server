93d80e8634310c630ba31838d2a53442b19fc073
Sunmin0520 <trymore87@gmail.com>
2020-07-23 17:47:32 +0900

    Fix typo in jwt

--- server/controller/users/signin.js
+++ server/controller/users/signin.js
@@ -14,14 +14,14 @@ module.exports = {
         if(user.dataValues.password === password){//db에 저장된 password = 유저가 입력한 password 일 때
           let payload = {id: user.id};
           let secret = process.env.JWT_SECRET;
-          jwt.sign(payload, secret, {expires:'2h'},(err,token) => {
+          jwt.sign(payload, secret, {expiresIn:'2h'},(err,token) => {
             if(err){
               res.status(500).send(err);
             } else {
               res.cookie('token',token);
-              res.status(201).json({id: user.dataValues.id})
+              res.status(201).json(user.dataValues)
             }
-          })
+          }) 
         } 
       } else {
         res.status(404).json({ result:'unvalid user' });//유저가 입력한 email이 db에 없을 때
