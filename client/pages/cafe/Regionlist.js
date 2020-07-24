0336058fc263cfc50569519e367fae30bf976ad5
ayus91 <jihaleeb1@gmail.com>
2020-07-24 17:02:42 +0900

    Fix axios

--- client/pages/cafe/Regionlist.js
+++ client/pages/cafe/Regionlist.js
@@ -1,38 +1,51 @@
-import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View, Button } from "react-native"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import axios from "axios";
 
 function Regionlist({ navigation }) {
   //DB에 있는 지역리스트를 가져옵니다.
+
+  const [region_id, Setregion_id] = useState("");
+  const [city, Setcity] = useState("");
+
+  const getRegionList = () => {
+    axios
+      .get("http://localhost:3001/cafes/regions")
+      .then((res) => {
+        return JSON.stringify(res.data);
+      })
+      .then((data) => {
+        return data.map((result) => {
+          key = result.id;
+          Setregion_id(result.id);
+          Setcity(result.name);
+        });
+      })
+      .catch(function (error) {
+        console.log(error); //401{result:"token expired"} 수정예정
+      });
+  };
+
+  useEffect(() => {
+    getRegionList();
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.textstyle}>어떤 지역의 카페정보가 궁금하신가요?</Text>
-      <View>
-        {axios
-          .get("http://localhost:3001/cafes/regions")
-          .then((res) =>
-            res.map((data) => (
-              <View key={data.id}>
-                {/* 각각의 지역 목록을 버튼화하여 선택시 해당 지역의 카페 목록으로 이동합니다 */}
-                <Button
-                  title={data.name}
-                  style={styles.textstyle}
-                  onPress={() => {
-                    navigation.navigate("Region", {
-                      region_id: data.id,
-                      city: data.name,
-                    })
-                  }}
-                ></Button>
-              </View>
-            ))
-          )
-          .catch(function (error) {
-            console.log(error) //401{result:"token expired"}
-          })}
-      </View>
+      {/* 각각의 지역 목록을 버튼화하여 선택시 해당 지역의 카페 목록으로 이동합니다 */}
+      <Button
+        title={city}
+        style={styles.textstyle}
+        onPress={() => {
+          navigation.navigate("Region", {
+            region_id: region_id,
+            city: city,
+          });
+        }}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -46,6 +59,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     margin: 10,
   },
-})
+});
 
-export default Regionlist
+export default Regionlist;
