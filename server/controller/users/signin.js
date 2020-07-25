225778d24ca99af8e9d6d54624fcde46648b36d9
Sunmin0520 <trymore87@gmail.com>
2020-07-26 07:44:48 +0900

    Update files related with jwt middleware(Not working yet)  - 기존에 커밋한 코드에는 req.body.user_id 부분이 있지만, 우리의 서비스에는 직접적으로 유저가 본인의 id를 req.body로 보내는 부분이 없어서 수정함  - 여전히 아예 verify단계를 거치지 않고 바로 401에러 나옴:

--- server/controller/users/signin.js
+++ server/controller/users/signin.js
@@ -3,59 +3,23 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = {
-  post: (req,res, next) => {
+  post: (req,res) => {
     const { email, password } = req.body;
-    console.log(email, password);
     users
     .findOne({ where: { email: email }})
     .then(user => {
-      if(user){
-        if(user.dataValues.password === password){//db에 저장된 password = 유저가 입력한 password 일 때
-          let payload = {id: user.id, username: user.name};//user: email, password 일치 단계 거쳐서 얻은 데이터
-          let secret = process.env.JWT_SECRET;
-          const token = jwt.sign(payload, secret, {expiresIn: '24h'});
-          res.status(200).json({
-            id:user.id, token: token
-          });
-        } 
-      } else {
-        res.status(404).json({ result:'unvalid user' });//유저가 입력한 email이 db에 없을 때
+      console.log('유저',user)
+      if(!user){
+        res.status(401).json({error:'입력된 이메일과 일치하는 사용자 없음'})
+      } 
+      if(user.dataValues.password !== password){
+        res.status(401).json({error:'비밀번호 틀림'})
       }
+      let payload = {id: user.dataValues.id, username: user.dataValues.username}
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '24h'});
+      res.status(200).json({
+        id:user.dataValues.id, token:token
+      })
     })
-    .catch(err => {
-      res.status(500).send(err);
-    })
-  }
-}
+  }}
 
-//jwt 미들웨어 적용 전
-// module.exports = {
-//   post: (req,res) => {
-//     const { email, password } = req.body;
-//     console.log(email, password);
-//     users
-//     .findOne({ where: { email: email }})
-//     .then(user => {
-//       if(user){
-//         //console.log('유저',user)
-//         if(user.dataValues.password === password){//db에 저장된 password = 유저가 입력한 password 일 때
-//           let payload = {id: user.id};
-//           let secret = process.env.JWT_SECRET;
-//           jwt.sign(payload, secret, {expiresIn:'2h'},(err,token) => {
-//             if(err){
-//               res.status(500).send(err);
-//             } else {
-//               res.cookie('token',token);
-//               res.status(201).json(user.dataValues)
-//             }
-//           })
-//         } 
-//       } else {
-//         res.status(404).json({ result:'unvalid user' });//유저가 입력한 email이 db에 없을 때
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send(err);
-//     })
-//   }
-// }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,20 +2,18 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = (req, res, next ) => {
-  console.dir('헤더',req.header.authorization)//아무 값도 안 나오고 있음
-  try{ 
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('decode된 토큰',decodedToken)//아예  언급이 안 되고 있음
-    const user_id = decodedToken.user_id;
-    if(req.body.user_id && req.body.user_id !== user_id){
-      throw 'Invalid user ID';
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).json(
-     'Invalid jwt'
-    )
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split('Bearer ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET , (err) => {
+      if (err) {
+        res.status(401).json({ error: 'Auth Error1 from auth' });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ error: 'Auth Error2 from auth' });
   }
+
 }
