f56332d4a10d02679d513c52ed2489d9ce43685a
Sunmin0520 <trymore87@gmail.com>
2020-07-25 12:31:58 +0900

    Add note.js(선택된 노트 세부사항 읽어오기)

--- server/controller/notes/index.js
+++ server/controller/notes/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   addNote: require('./addNote'),
-  noteList: require('./noteList')
+  noteList: require('./noteList'),
+  note: require('./note')
 }
 
 //index.js에서는 파일 경로 지정 / routes에서 엔드포인트 설정
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -5,5 +5,6 @@ const { notesController } = require('../controller');
 
 router.post('/',notesController.addNote.post)
 router.get('/',notesController.noteList.get)
+router.get('/:note_id', notesController.note.get)
 
 module.exports = router;
