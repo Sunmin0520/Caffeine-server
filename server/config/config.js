0780847833f3d9eabbe7ce233098ae369b0f1e50
Sunmin0520 <trymore87@gmail.com>
2020-07-27 23:06:50 +0900

    Update username for RDS in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 //사용할 정보들을 개발용, 테스트용, 배포용으로 나누었습니다. 추후 업데이트 할 예정입니다. (port 추가,host 변경 등)
   const development =  {
-    username: "root",
+    username: "sunmin",
     password: process.env.DATABASE_PASSWORD,
     database: "caffeine",
     host: "caffeine.cxc8xblv1vqn.ap-northeast-2.rds.amazonaws.com",
