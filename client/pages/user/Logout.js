ffd2a83bbd921806e8c883dfd17a255aedc15afe
김현우 <hyun0310woo@gmail.com>
2020-07-28 15:36:44 +0900

    Add Logout page & navigation

---
+++ client/pages/user/Logout.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet, Button, Alert } from "react-native";
+
+const Logout = ({ navigation }) => {
+  useEffect(() => {
+    logoutFunc();
+  });
+
+  const logoutFunc = () =>
+    Alert.alert(
+      "Log out",
+      "로그아웃 하시겠습니까?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => navigation.navigate("userInfo") },
+      ],
+      { cancelable: false }
+    );
+
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
+
+export default Logout;
