fd8495768833a0421e555fbf19575d3c9f4d810d
Sunmin0520 <trymore87@gmail.com>
2020-07-28 23:16:55 +0900

    Update config for development

--- server/config/config.js
+++ server/config/config.js
@@ -1,31 +1,39 @@
 require('dotenv').config();
-
-  const development = {
+  const development =  {
     username: "root",
-    password: process.env.DATABASE_PASSWORD,
+    password: process.env.DATABASE_PASSWORD1,
     database: "caffeine",
     host: "127.0.0.1",
     dialect: "mysql",
     logging: false
   };
 
-  const test = {
-    username: "root",
-    password: process.env.DATABASE_PASSWORD,
-    database: "caffeine",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    logging: false
-  };
+  // const development =  {
+  //   username: "sunmin",
+  //   password: process.env.DATABASE_PASSWORD2,
+  //   database: "caffeine",
+  //   host: "caffeine.cxc8xblv1vqn.ap-northeast-2.rds.amazonaws.com",
+  //   port: 3306,
+  //   dialect: "mysql",
+  //   logging: false,
+  // };
+  
+  // const development = {
+  //   username: "root",
+  //   password: process.env.DATABASE_PASSWORD,
+  //   database: "caffeine",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql",
+  //   logging: false
+  // };
 
-  const production =  {
-    username: "sunmin",
-    password: process.env.DATABASE_PASSWORD,
-    database: "caffeine",
-    host: "caffeine.cxc8xblv1vqn.ap-northeast-2.rds.amazonaws.com",
-    port: 3306,
-    dialect: "mysql",
-    logging: false,
-  };
+  // const test = {
+  //   username: "root",
+  //   password: process.env.DATABASE_PASSWORD,
+  //   database: "caffeine",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql",
+  //   logging: false
+  // };
 
 module.exports = { development, test, production};
