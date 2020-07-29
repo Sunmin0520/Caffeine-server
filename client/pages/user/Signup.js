d7c513d241cbd95509edc239dde062b2eab6a00a
김현우 <hyun0310woo@gmail.com>
2020-07-29 14:54:48 +0900

    Update Signup.js Button code

--- client/pages/user/Signup.js
+++ client/pages/user/Signup.js
@@ -5,21 +5,49 @@ import axios from "axios";
 
 export default function Signup({ navigation }) {
   const [email, setEmail] = useState(""); // 이메일 인풋값 핸들링
-  const [nickname, setNickname] = useState(""); // 닉네임 인풋값 핸들링
+  const [username, setNickname] = useState(""); // 닉네임 인풋값 핸들링
   const [password, setPassword] = useState(""); // 패스워드 인풋값 핸들링
   const [passwordCheck, setPasswordCheck] = useState(""); // 패스워드체크 인풋값 핸들링
 
-  function doesPasswordMatch() {
-    return password === passwordCheck;
-  }
+  const postSignupData = () => {
+    console.log(`이메일: ${email} 패스워드: ${password} 닉네임: ${username}`);
+    const data = JSON.stringify({
+      email: email,
+      password: password,
+      username: username,
+    });
 
-  function renderFeedbackMessage() {
+    const config = {
+      method: "post",
+      url: "http://13.125.247.226:3001/users/signup",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(response.data);
+        alert(`${email}님 회원가입 하셨습니다. 로그인 페이지로 이동합니다.`);
+        navigation.navigate("Signin");
+      })
+      .catch((error) => {
+        console.log(`${error} 에러 ${data}를 보내지 못했습니다.`);
+      });
+  };
+
+  const doesPasswordMatch = () => {
+    return password === passwordCheck;
+  };
+
+  const renderFeedbackMessage = () => {
     if (passwordCheck) {
       if (!doesPasswordMatch()) {
         return <Text>패스워드가 일치하지 않습니다!!</Text>;
       }
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -29,12 +57,14 @@ export default function Signup({ navigation }) {
         placeholder="   Email"
         style={styles.input}
         onChangeText={(email) => setEmail(email)}
+        autoCapitalize="none"
       />
       <TextInput
         label="Text"
         placeholder="   Nickname"
         style={styles.input}
-        onChangeText={(nickname) => setNickname(nickname)}
+        onChangeText={(username) => setNickname(username)}
+        autoCapitalize="none"
       />
       <TextInput
         label="Password"
@@ -42,6 +72,7 @@ export default function Signup({ navigation }) {
         secureTextEntry={true}
         style={styles.input}
         onChangeText={(password) => setPassword(password)}
+        autoCapitalize="none"
       />
       <TextInput
         label="Password"
@@ -49,41 +80,20 @@ export default function Signup({ navigation }) {
         secureTextEntry={true}
         style={styles.input}
         onChangeText={(passwordCheck) => setPasswordCheck(passwordCheck)}
+        autoCapitalize="none"
       />
       <View>{renderFeedbackMessage()}</View>
       <Button
         title="회원가입"
-        onPress={() => {
-          console.log(
-            `이메일: ${email} 패스워드: ${password} 닉네임: ${nickname}`
-          );
-          const data = JSON.stringify({
-            email: email,
-            password: password,
-            nickname: nickname,
-          });
-
-          const config = {
-            method: "post",
-            url: "http://13.125.247.226:3001/users/signup",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            data: data,
-          };
-
-          axios(config)
-            .then((response) => {
-              console.log(response.data);
-              alert(
-                `${email}님 회원가입 하셨습니다. 로그인 페이지로 이동합니다.`
-              );
-              navigation.navigate("Signin");
-            })
-            .catch((error) => {
-              console.log(`${error} 에러 ${data}를 보내지 못했습니다.`);
-            });
-        }}
+        onPress={postSignupData}
+        disabled={
+          email === "" ||
+          username === "" ||
+          password === "" ||
+          passwordCheck === ""
+            ? true
+            : false
+        }
       />
     </View>
   );
