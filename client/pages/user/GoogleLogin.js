efc237d31da6fff8b5ddd05d22bc3fedbfa27f9e
김현우 <hyun0310woo@gmail.com>
2020-07-31 02:14:41 +0900

    Add Google Login

--- client/pages/user/GoogleLogin.js
+++ client/pages/user/GoogleLogin.js
@@ -1,42 +1,46 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import * as GoogleSignIn from "expo-google-sign-in";
+import { Text, View, StyleSheet, Button, AsyncStorage } from "react-native";
+import * as Google from "expo-google-app-auth";
+import { useNavigation } from "@react-navigation/native";
 
-export default function GoogleLogin() {
-  signInWithGoogle = async () => {
+const ANDROID_CLIENT_ID =
+  "348379217678-iep3tgg5rfud3s2be2psvql59d74g10u.apps.googleusercontent.com";
+
+function GoogleLogin() {
+  const navigation = useNavigation();
+  const signInWithGoogleAsync = async () => {
     try {
-      const result = await GoogleSignIn.logInAsync({
-        androidClientId:
-          "348379217678-iep3tgg5rfud3s2be2psvql59d74g10u.apps.googleusercontent.com",
-        success: ["profile", "email"],
+      const result = await Google.logInAsync({
+        androidClientId: ANDROID_CLIENT_ID,
+        scopes: ["profile", "email"],
       });
 
       if (result.type === "success") {
-        console.log(result.user.givenName);
-        // console.log(result);
+        // console.log("LoginGoogle: ", result);
+        const _storeData = async () => {
+          try {
+            await AsyncStorage.setItem("userToken", result.accessToken);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        _storeData();
+        navigation.navigate("Tab");
         return result.accessToken;
       } else {
         return { cancelled: true };
       }
-    } catch (error) {
-      console.log("Err ", error);
+    } catch (e) {
+      console.log("에러: ", e);
       return { error: true };
     }
   };
 
   return (
     <View>
-      <Text onPress={this.signInWithGoogle}>
-        {/* <Image style={styles.tinyLogo} source={require("./google.png")} /> */}
-        Google Login
-      </Text>
+      <Button title="Login with Google" onPress={signInWithGoogleAsync} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  tinyLogo: {
-    width: 15,
-    height: 15,
-  },
-});
+export default GoogleLogin;
