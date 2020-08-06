abb099a96f580de01c1fd86e31b497c79a805f67
Sunmin0520 <trymore87@gmail.com>
2020-08-06 20:37:36 +0900

    Delete code related with image

--- server/routes/cafes.js
+++ server/routes/cafes.js
@@ -1,12 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const multer = require("multer");
-
-// // 1. multer 미들웨어 등록
-// let upload = multer({
-//     dest: "upload/"
-// })
-
 
 const auth = require('../middleware/auth');
 const { cafesController } = require('../controller');
@@ -20,9 +13,5 @@ router.get('/rating/:cafe_id', auth, cafesController.avgRating.get);
 router.post('/bookmark/:cafe_id', auth, cafesController.addBookmark.post);
 router.get('/bookmark/all', auth, cafesController.bookmark.get);
 router.delete('/bookmark/:bookmark_id', auth, cafesController.deleteBookmark.delete);
-//router.post('/image', cafesController.uploadImage.post);
-// router.post('/upload', upload.array('photos', 3), function(req, res, next) {
-//   res.send('Successfully uploaded ' + req.files.length + ' files!')
-// })
 
 module.exports = router;
